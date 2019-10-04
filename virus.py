13aad3d48cf66a0b0683bbfc571f4d636c0d3dd0
clenberger <christian.lenberger@gmail.com>
2019-10-03 19:32:44 -0700

    worked on cleaning up files

--- virus.py
+++ virus.py
@@ -1,15 +1,16 @@
 class Virus(object):
-    '''Properties and attributes of the virus used in Simulation.'''
+    """Properties and attributes of the virus used in Simulation."""
 
     def __init__(self, name, repro_rate, mortality_rate):
+        """Initialize starting values"""
         self.name = name
         self.repro_rate = repro_rate
         self.mortality_rate = mortality_rate
 
 
 def test_virus_instantiation():
-    #TODO: Create your own test that models the virus you are working with
-    '''Check to make sure that the virus instantiator is working.'''
+    # TODO: Create your own test that models the virus you are working with
+    """Check to make sure that the virus instantiator is working."""
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
